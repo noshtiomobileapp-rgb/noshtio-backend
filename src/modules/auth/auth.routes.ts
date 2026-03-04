27c9ecabd949f2f376f36d877f7e93337e5baea1
noshtiomobileapp-rgb <noshtiomobileapp@gmail.com>
2026-03-04 09:48:28 +0530

    Update auth.routes.ts

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, login, logout } from "./auth.controller";
+import { register, login, logout, getCurrentUser } from "./auth.controller";
+import { requireAuth } from "../../middleware/requireAuth";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/current", requireAuth, getCurrentUser);
 
 export default router;
